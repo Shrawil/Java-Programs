1b7f28ae526b607f467bfe35861b9933d2d7fb0d
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-02-09 00:42:01 +0530

    Created first java program.

---
+++ first.java
@@ -0,0 +1,15 @@
+class first {
+    public static void main(String[] args) {
+        //Comments in Java
+        int num = 10;
+        double dec = 11.11;
+        char opr = '+';
+        String name = "Shrawil";
+        //println adds a \n at the end so next print statement moves to next line.
+        System.out.println("Num : " + num);
+        System.out.println("Dec = " + dec);
+        System.out.println("Opr = " + opr);
+        //print add no next line at the end.
+        System.out.print("Name : " + name);
+    }
+}
