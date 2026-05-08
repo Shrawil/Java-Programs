dbc171e82e24b3192138d7690f23ee70e9a583eb
Shrawil <shrawilsrivastavaa@gmail.com>
2026-05-08 23:01:29 +0530

    Added input in first.java

--- first.java
+++ first.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 class first {
     public static void main(String[] args) {
-        //Comments in Java
-        int num = 10;
-        double dec = 11.11;
-        char opr = '+';
-        String name = "Shrawil";
-        //println adds a \n at the end so next print statement moves to next line.
+
+        // Comments in Java
+
+        int num = 10; // Integer variable 
+        double dec = 11.11; // Decimal variable
+        char opr = '+'; // Character variable
+        String name = "Shrawil"; // String is a set of characters terminated by a NULL character.
+
+        // println adds a \n at the end so next print statement moves to next line.
         System.out.println("Num : " + num);
         System.out.println("Dec = " + dec);
         System.out.println("Opr = " + opr);
         //print add no next line at the end.
         System.out.print("Name : " + name);
+
+        // Taking input in Java
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nEnter your name : ");
+        name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
+
     }
 }
